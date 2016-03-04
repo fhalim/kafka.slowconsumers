d3bac7047f25ab82c0d3620a378c39ad62616819
Fawad Halim <fawad@fawad.net>
2016-03-03 19:46:22 -0600

    Make consumer even slower; reduced fetch bytes to ensure all consumers fetch some records

--- src/main/scala/net/fawad/learningkafka/Main.scala
+++ src/main/scala/net/fawad/learningkafka/Main.scala
@@ -13,9 +13,9 @@ import scala.util.Random
 
 object Main extends App {
   val props = new Properties()
-  props.putAll(Map("bootstrap.servers" -> "localhost:9092", "group.id" -> "learningkafka"))
+  props.putAll(Map("bootstrap.servers" -> "localhost:9092", "group.id" -> "learningkafka", "max.partition.fetch.bytes"-> "1024"))
   val consumers = 5
-  val sleepDuration = 30000
+  val sleepDuration = 60000
   import scala.concurrent.ExecutionContext.Implicits.global
   for (producer <- managed(new KafkaProducer[String, String](props, new StringSerializer, new StringSerializer))) {
     val futures = for (c <- Range(0, consumers))
@@ -23,7 +23,7 @@ object Main extends App {
         for (consumer <- managed(new KafkaConsumer[String, String](props, new StringDeserializer, new StringDeserializer))) {
           consumer.subscribe(List("learningkafka"))
           while (true) {
-            val messageFromTopic = consumer.poll(30000)
+            val messageFromTopic = consumer.poll(1000)
             for (m <- messageFromTopic) {
               println(s"Consumer ${c}: Message: ${m.value()}")
             }
@@ -32,7 +32,7 @@ object Main extends App {
         }
       }
     while(true){
-      val producerSleep = sleepDuration/consumers
+      val producerSleep = sleepDuration/consumers/2
       producer.send(new ProducerRecord[String, String]("learningkafka", s"foo${Random.nextInt()}", s"bar${Random.nextInt()}"))
       Thread.sleep(producerSleep)
     }
